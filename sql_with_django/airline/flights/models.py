10207d51b1a21a63d5570b83cdf9bdd670445e1d
onlyVishesh <Visheshmaurya14@gmail.com>
2023-03-27 00:45:07 +0530

    Added sql_with_django folder

---
+++ sql_with_django/airline/flights/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+# Create your models here.
+
+class Airport(models.Model):
+    code = models.CharField(max_length=3)
+    city = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.city} ({self.code})"
+
+
+class Flight(models.Model):
+    origin = models.ForeignKey(Airport,on_delete=models.CASCADE,related_name="departure")
+    destination = models.ForeignKey(Airport,on_delete=models.CASCADE,related_name="arrivals")
+    duration = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.id}: {self.origin} to {self.destination}"
+
+class Passenger(models.Model):
+    fname = models.CharField(max_length=32)
+    lname = models.CharField(max_length=32)
+    age = models.IntegerField()
+    flights = models.ManyToManyField(Flight, blank=True, related_name="passengers")
+
+    def __str__(self):
+        return f"{self.fname} {self.lname}"
+

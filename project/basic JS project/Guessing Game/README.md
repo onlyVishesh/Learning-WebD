5be2df17411272f8156b1faa845bdafb3a6f8490
onlyVishesh <Visheshmaurya14@gmail.com>
2023-09-13 15:43:46 +0530

    Changed file location

--- project/basic JS project/Guessing Game/README.md
+++ project/basic JS project/Guessing Game/README.md
@@ -1,4 +1,25 @@
-# Guessing Number Game
+
+
+<h1 align="center">Project Name - Guessing Number Game
+</h1>
+<!-- ABOUT THE PROJECT -->
+
+## About The Project
+This a small project created to learn JavaScript concepts. In this project i learn about conditional statements and functions with DOM manuplation.
+
+Demo Video - 
+
+https://github.com/onlyVishesh/Learning-WebD/assets/121187728/7f5b2df3-947b-416a-b84a-8013652100d0
+
+
+
+### Built With
+
+This project is build using - 
+
+* ![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+* ![HTML](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white) 
+* ![CSS3](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white) 
 
 ### Rules -
 
--- project/basic JS project/counter/index.js
+++ project/basic JS project/counter/index.js
@@ -1,18 +1,22 @@
 let count = 0;
 
-document.getElementById("dec").onclick = () => {
-    count -= 1;
-    document.getElementById("count").innerHTML = count;
-}
+document.querySelectorAll(".btns").forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        if (e.currentTarget.classList.contains("dec")) {
+            count--;
+        } else if (e.currentTarget.classList.contains("inc")) {
+            count++;
+        } else if (e.currentTarget.classList.contains("reset")) {
+            count = 0;
+        }
 
-document.getElementById("reset").onclick = () => {
-    count = 0;
-    document.getElementById("count").innerHTML = count;
-
-}
-
-document.getElementById("inc").onclick = () => {
-    count += 1;
-    document.getElementById("count").innerHTML = count;
-
-}
+        document.querySelector(".count").innerText = count;
+        if (count < 0) {
+            document.querySelector(".count").style.color = "red";
+        } else if (count > 0) {
+            document.querySelector(".count").style.color = "green";
+        } else {
+            document.querySelector(".count").style.color = "black";
+        }
+    });
+});

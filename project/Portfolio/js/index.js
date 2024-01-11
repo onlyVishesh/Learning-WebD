9b4c7636ac0ba7c18eb3feb63cd0fc85dc1ad5b8
onlyVishesh <Visheshmaurya14@gmail.com>
2024-01-11 23:42:27 +0530

    Added light mode

--- project/Portfolio/js/index.js
+++ project/Portfolio/js/index.js
@@ -34,6 +34,15 @@ const pageTransition = () => {
       element.classList.add("active");
     }
   });
+
+  // Toggle theme
+
+  const themeBtn = document.querySelector(".theme-btn");
+  themeBtn.addEventListener("click", () => {
+    let element = document.body;
+    element.classList.toggle("light-mode");
+  });
+
 };
 
 pageTransition();
